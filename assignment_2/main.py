e87f42e0d38858600a0093ca3f9c1865a78ff002
Jason Russell <jasrusable@gmail.com>
2015-04-19 14:21:12 +0200

    jesus

--- assignment_2/main.py
+++ assignment_2/main.py
@@ -24,34 +24,38 @@ def get_point_by_name(name, points):
         raise Exception('Point {0} not found.'.format(name))
     return wanted_point
 
+def get_provisional_points(points):
+    provisional_points = []
+    for point in points:
+        if point.type_ == 'P':
+            provisional_points.append(point)
+    return provisional_points
+
 points = get_list_of_points_from_file(path='data/points.csv')
 
 observations = get_list_of_observations_from_file(path='data/observations.csv')
 
 set_up_point_names = get_set_up_points_names(observations)
 
-
 A = numpy.matrix([
-        [0, 0, 0, 0, 0, 0] + [0] * len(set_up_point_names),
+        [0,0,0,0,0,0,0,0,0,0,0],
     ])
 A = numpy.delete(A, (0), axis=0)
 
-A_row = [0, 0, 0, 0, 0, 0] + [0] * len(set_up_point_names)
-i = 0
-for point_name in set_up_point_names:
-    for observation in observations:
-        if isinstance(observation, DirectionObservation):
-            from_point = get_point_by_name(observation.from_point_name, points)
-            to_point = get_point_by_name(observation.to_point_name, points)
-            if to_point.type_ == 'P':
-                d = get_distance(from_point, to_point)
-                y = 206264.8 * (to_point.x - from_point.x) / d**2
-                x = -206264.8 * (to_point.y - from_point.y) / d**2
-                if to_point.name == 'SUR10':
-                    A_row[0], A_row[1] = y, x
-                A = numpy.vstack([A, A_row])
-    i += 1
-
-
+for observation in observations:
+    if isinstance(observation, DirectionObservation):
+        A_row = [0,0,0,0,0,0,0,0,0,0,0]
+        from_point = get_point_by_name(observation.from_point_name, points)
+        to_point = get_point_by_name(observation.to_point_name, points)
+        i = 0
+        for unknown_point in get_provisional_points(points):
+            if to_point == unknown_point:
+                A_row[i] = 1
+                A_row[i+1] = 1
+            if from_point == unknown_point:
+                A_row[i] = 1
+                A_row[i+1] = 1
+            i += 2
+        A = numpy.vstack([A, A_row])
 
 print(A)
